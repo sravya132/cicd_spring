f325070327cd5fa19ddec97e700fe0e3b3a123cd
sravyagoud <sravya.goud16@gmail.com>
2025-08-21 09:27:34 +0530

    Initial commit

---
+++ cicd_spring/src/main/java/com/example/cicd_spring/CicdSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicd_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdSpringApplication.class, args);
+	}
+
+}
